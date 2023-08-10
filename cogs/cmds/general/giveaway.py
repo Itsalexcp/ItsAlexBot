912fc4c214940808fe4a294f8de0a766ca6c1abe
Itsalexcp <itsalex.cp@gmail.com>
2023-08-11 00:46:27 +0200

    giveaway

--- cogs/cmds/general/giveaway.py
+++ cogs/cmds/general/giveaway.py
@@ -99,3 +99,102 @@ class Giveaway(commands.Cog):
         msg = await channel.send(embed=embed, view=giveaway_on())
         await ctx.send(f"Giveaway started in {channel.mention}!", ephemeral=True)
         await asyncio.sleep(tim)
+
+        if application == 1:
+            await msg.edit(view=giveaway_appli())
+        else:
+            await msg.edit(view=giveaway_off())
+        users = await msg.reactions[0].users().flatten()
+        users.pop(users.index(self.client.user))
+        if len(users) < winners:
+            winners = len(users)
+        winner = random.sample(users, winners)
+        await channel.send(f"Congratulations {winner}! You won {prize}!", delete_after=delete_time)
+        await msg.edit(view=None)
+        await asyncio.sleep(delete_time - round(time.time()))
+        await msg.delete()
+
+    @giveaway.sub_command(name="end", description="End a giveaway")
+    async def giveaway_end(self, ctx, channel: disnake.TextChannel, message_id: int):
+        """
+                Parameters
+                ----------
+                channel : The channel where the giveaway is hosted
+                message_id : The message ID of the giveaway
+                :param message_id:
+                :param channel:
+                :param ctx:
+        """
+        msg = await channel.fetch_message(message_id)
+        await msg.edit(view=giveaway_off())
+        users = await msg.reactions[0].users().flatten()
+        users.pop(users.index(self.client.user))
+        winner = random.choice(users)
+        await channel.send(f"Congratulations {winner.mention}! You won the giveaway!", delete_after=604800)
+        await msg.edit(view=None)
+        await asyncio.sleep(604800)
+        await msg.delete()
+
+    @giveaway.sub_command(name="delete", description="Delete a giveaway")
+    async def giveaway_delete(self, ctx, channel: disnake.TextChannel, message_id: int):
+        """
+                Parameters
+                ----------
+                channel : The channel where the giveaway is hosted
+                message_id : The message ID of the giveaway
+                :param message_id:
+                :param channel:
+                :param ctx:
+        """
+        msg = await channel.fetch_message(message_id)
+        await msg.edit(view=giveaway_off())
+        await msg.edit(view=None)
+        await msg.delete()
+
+    @giveaway.sub_command(name="reroll", description="Reroll a giveaway")
+    async def giveaway_reroll(self, ctx, channel: disnake.TextChannel, message_id: int):
+        """
+                Parameters
+                ----------
+                channel : The channel where the giveaway is hosted
+                message_id : The message ID of the giveaway
+                :param message_id:
+                :param channel:
+                :param ctx:
+        """
+        msg = await channel.fetch_message(message_id)
+        users = await msg.reactions[0].users().flatten()
+        users.pop(users.index(self.client.user))
+        winner = random.choice(users)
+        await channel.send(f"Congratulations {winner.mention}! You won the giveaway!", delete_after=604800)
+        await msg.edit(view=None)
+        await asyncio.sleep(604800)
+        await msg.delete()
+
+    @giveaway.sub_command(name="list", description="List all giveaways")
+    async def giveaway_list(self, ctx):
+        """
+                Parameters
+                ----------
+                ctx :
+                :param ctx:
+        """
+        pass
+
+    @giveaway.sub_command(name="info", description="Get info about a giveaway")
+    async def giveaway_info(self, ctx, channel: disnake.TextChannel, message_id: int):
+        """
+                Parameters
+                ----------
+                channel : The channel where the giveaway is hosted
+                message_id : The message ID of the giveaway
+                :param message_id:
+                :param channel:
+                :param ctx:
+        """
+        msg = await channel.fetch_message(message_id)
+        await ctx.send(f"**Prize:** {msg.embeds[0].title}\n**Description:** {msg.embeds[0].description}\n**Ended:** <t:{round(msg.embeds[0].fields[0].value.timestamp)}:D> (<t:{round(msg.embeds[0].fields[0].value.timestamp)}:R>)\n**Winners:** {msg.embeds[0].fields[1].value}\n**Hosted by:** {msg.embeds[0].fields[2].value}\n**Message ID:** {message_id}", ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(Giveaway(bot))
